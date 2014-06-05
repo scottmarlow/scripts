266c6bd3600c5bcbb0b3acd26c8339f8633e099e
Scott Marlow <smarlow@redhat.com>
2014-06-05 10:26:07 -0400

    add separate parameter for foldername

--- bash/tck_getlogszip.sh
+++ bash/tck_getlogszip.sh
@@ -2,13 +2,13 @@
 
 current = $PWD
 
-if [ ! -d $1 ]; then
-    mkdir $1
+if [ ! -d $2 ]; then
+    mkdir $2
 fi
-cd $1
-echo "downloading for $1"
+cd $2
+echo "downloading for $2"
 curl -O $1/ws/work/logs/*zip*/logs.zip
-echo "download complete for $1, time to unzip"
+echo "download complete for $2, time to unzip"
 unzip -o logs.zip
-echo "processing complete for $1"
+echo "processing complete for $2"
 cd $current
